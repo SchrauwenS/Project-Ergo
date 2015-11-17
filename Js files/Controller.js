a1b4b2aa840885398b08620037f0e025f783588a
V4mirax <kerstensbart.bk@gmail.com>
2015-11-17 16:43:09 +0100

    Able to change webpage

--- Js files/Controller.js
+++ Js files/Controller.js
@@ -1,33 +1,29 @@
-﻿var app = angular.module('mainApp', ['ngRoute']);
+﻿    var app = angular.module('mainApp', ['ngRoute']);
 
-app.config(function ($routeProvider) {
-    $routeProvider
-    .when('/Html/', {
-        templateUrl: '/Html/'
-    })
-    .when('/Html/dasboard', {
-        templateUrl: '/Html/'
-    })
-    .when('/Html/Index', {
-        templateUrl: '/Html/'
-    })
-    .otherwhise({
-        redirectTo: '/Html/'
+    app.config(function($routeProvider){
+        
+        $routeProvider
+        .when('/',
+        {
+            templateUrl: "/login.html"
+        })
+            .when('/Questions',
+        {
+            templateUrl: "/Questions.html"
+        })
+        .otherwise({
+           redirectTo:'/'
+        });
+
     });
 
+    app.controller('loginCtrl', function ($scope,$location) {
+        $scope.submit = function () {
+            var uname = $scope.username;
+            var password = $scope.password;
 
-});
-
-app.controller('loginCtrl', function ($scope) {
-
-    $scope.submit = function () {
-        var uname = $scope.username;
-        var password = $scope.password;
-
-        if ($scope.username == 'admin' && $scope.password == 'admin') {
-            $location.path('Html/Index');
+            if ($scope.username == 'admin' && $scope.password =='admin') {
+                $location.path('/Questions')
+            }
         }
-
-    };
-
-});
+    })
