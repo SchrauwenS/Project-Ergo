bf8592203def3b574b6949614f0747f5ab741487
V4mirax <kerstensbart.bk@gmail.com>
2015-11-17 11:34:13 +0100

    angular enzo

---
+++ Js files/Controller.js
@@ -0,0 +1,31 @@
+﻿var app = angular.module('mainApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl:'login.html'
+    })
+    .when('/dasboard', {
+        templateUrl: 'login.html'
+    })
+    
+    .otherwhise({
+        redirectTo: '/'
+    });
+
+
+});
+
+app.controller('loginCtrl', function ($scope) {
+
+    $scope.submit = function () {
+        var uname = $scope.username;
+        var password = $scope.password;
+
+        if ($scope.username == 'admin' && $scope.password == 'admin') {
+            $location.path('/dashboard');
+        }
+
+    };
+
+})
