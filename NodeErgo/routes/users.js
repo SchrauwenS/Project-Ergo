308051b7129e489b56214db81cf85bb6a03446bf
V4mirax <kerstensbart.bk@gmail.com>
2015-11-19 16:17:02 +0100

    Fixing inlog screen

--- NodeErgo/routes/users.js
+++ NodeErgo/routes/users.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET Userlist page. */
+/*
+ * GET userlist.
+ */
 router.get('/', function (req, res) {
     var db = req.db;
     var collection = db.get('Users');
     collection.find({}, {}, function (e, docs) {
-        res.render('users', {
-            "users" : docs
-        });
+        res.json(docs);
     });
 });
 
