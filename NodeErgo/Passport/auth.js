a37ca96c9ecaf9a03a081571ea1166a7570ab90a
V4mirax <kerstensbart.bk@gmail.com>
2015-11-29 15:42:17 +0100

    Bezig met authentication

---
+++ NodeErgo/Passport/auth.js
@@ -0,0 +1,24 @@
+﻿var passport = require('passport');
+
+
+exports.authenticate = function (req, res, next) {
+    
+    var auth = passport.authenticate('local', function (err, username) {
+        
+        if (err) {
+            return next(err);
+        }
+        if (!username) {
+            console.log('no success');
+            res.send({ success: false });
+        }
+        req.logIn(username, function (err) {
+            if (err) {
+                return next(err);
+            }
+            res.send({ success: true, username: username });
+        })
+        
+    })
+    auth(req, res, next);
+}
--- NodeErgo/routes/index.js
+++ NodeErgo/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyparser = require('body-parser');
-var passport = require('passport');
+var auth = require('../Passport/auth.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,27 +9,7 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/login', function (req, res, next){
-    
-    var auth = passport.authenticate('local', function (err, username){
-              
-        if (err) {
-            return next(err);
-        }
-        if (!username) {
-            console.log('no success');
-            res.send({ success: false });
-        }
-        req.logIn(username, function (err){
-            if (err) {
-                return next(err);
-            }
-            res.send({ success: true,username:username });
-        })
-        
-    })
-    auth(req, res, next);
-});
+router.post('/login', auth.authenticate);
 
 
 
