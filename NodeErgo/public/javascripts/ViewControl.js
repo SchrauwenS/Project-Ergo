9087bd6f847d75de3ce57d22e50658bf081ad62f
Stijn Schrauwen <stijnschrauwen1995@hotmail.com>
2015-12-07 15:06:34 +0100

    naam aanpassing

---
+++ NodeErgo/public/javascripts/ViewControl.js
@@ -0,0 +1,17 @@
+﻿(function () {
+
+    var App = angular.module("App", ["ngRoute"]);
+
+    App.config(function ($routeProvider){
+        $routeProvider
+            .when("/Questions", {
+                templateUrl: "views/Questions.html",
+                controller: "MainCtrl"
+            })
+            .when("/Endscreen", {
+            templateUrl: "views/Endscreen.html",
+            controller: "MainCtrl"
+            })
+            .otherwise({ redirectTo: "/" });//indien pagina niet gevonden word
+    });
+}());
