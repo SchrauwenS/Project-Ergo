424319e0f9ba5ce99e2b8e54da3ae0b120e8cdea
V4mirax <kerstensbart.bk@gmail.com>
2015-12-15 10:00:34 +0100

    Updated admin powers to the max

--- NodeErgo/routes/admin.js
+++ NodeErgo/routes/admin.js
@@ -1,7 +1,12 @@
 ﻿var express = require('express');
 var router = express.Router();
 var schema = require('../Mongoose/gebruiker');
+
+
 var vragen = schema.survey;
+var user = schema.Users;
+var scores = schema.subScore;
+
 
 var bodyparser = require('body-parser');
 var auth = require('../Passport/auth.js')
@@ -15,12 +20,41 @@ router.get('/', isAdmin, function (req, res, next) {
     res.redirect('users/users');
 });
 
-router.get('/users:userID', isAdmin, function (req, res, next) {
-    var userID = req.params.userID;
+
+/* Get specific user*/
+router.get('/users/:id', isAdmin, function (req, res, next) {
+    var userID = req.params.id;
+    console.log(userID);
+    user.findById(userID).lean().exec(function (err, result) {
+        if (err) return console.error(err);
+        
+        res.status(200).json(result);
+    })
+
+});
+/* get specific Survey from a user */
+router.get('/users/:id/survey', isAdmin, function (req, res, next) {
+    var userID = req.params.id;
+    console.log(userID);
+    vragen.find({ user: userID }).lean().exec(function (err, result) {
+        if (err) return console.error(err);
+        
+        res.status(200).json(result);
+    })
+
 });
 
+/* get specific Survey from a user */
+router.get('/users/:id/score', isAdmin, function (req, res, next) {
+    var userID = req.params.id;
+    console.log(userID);
+    scores.find({ user: userID }).lean().exec(function (err, result) {
+        if (err) return console.error(err);
+        
+        res.status(200).json(result);
+    })
 
-
+});
 
 
 module.exports = router;
