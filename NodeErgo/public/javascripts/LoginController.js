35b9589b0a51c4e4a0be14a29087b2730c523779
Stijn Schrauwen <stijnschrauwen1995@hotmail.com>
2015-12-16 23:16:15 +0100

    Login pagina aangemaakt met aparte wrapper en controllers

---
+++ NodeErgo/public/javascripts/LoginController.js
@@ -0,0 +1,11 @@
+﻿(function () {
+
+var App = angular.module("App");
+var LoginCtrl = function ($scope, $rootScope, $location, AuthenticationService) {
+        
+
+        App.controller("LoginCtrl", LoginCtrl);
+
+};
+
+})();
