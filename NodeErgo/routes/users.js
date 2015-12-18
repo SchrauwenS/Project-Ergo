6455c574d46d6cad3ca7761b33fba717c8ca550d
V4mirax <kerstensbart.bk@gmail.com>
2015-12-18 15:04:05 +0100

    blablabla

--- NodeErgo/routes/users.js
+++ NodeErgo/routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bodyparser = require('body-parser');
-
+var UserC = require('../Controllers/UserController');
 var auth = require('../passport/auth');
 
 
