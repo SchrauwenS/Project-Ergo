13b28e80edf13cac6480a3dd03c5a09cae62b052
Stijn Schrauwen <stijnschrauwen1995@hotmail.com>
2015-12-23 20:57:37 +0100

    admin page userinfo + userscore

--- NodeErgo/public/javascripts/AdminController.js
+++ NodeErgo/public/javascripts/AdminController.js
@@ -2,10 +2,19 @@
     var App = angular.module("App");
     var AdminCtrl = function ($scope, $routeParams, $location, $http) {
         
-        $http.get('users')
+        $http.get('admin/users')
             .then(function (res) {
             $scope.Userlist = res.data;
         });
+
+        $scope.GetData = function () {
+              $http.get("admin/users/" + $scope.selectedUSER[0]._id + "/score")
+                    .then(function (res) {
+                    $scope.scorelist = res.data;
+                    });
+            }
+           
+                    
     };
     App.controller("AdminCtrl", AdminCtrl);
     
