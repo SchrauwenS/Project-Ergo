813ed70c8ddce05ad52bbf2e14ce0d8def5432b9
Stijn Schrauwen <stijnschrauwen1995@hotmail.com>
2015-12-30 15:38:51 +0100

    logout

--- NodeErgo/public/javascripts/AdminController.js
+++ NodeErgo/public/javascripts/AdminController.js
@@ -1,27 +1,31 @@
 ﻿(function () {
     var App = angular.module("App");
-    var AdminCtrl = function ($scope, $routeParams, $location, $http) {
+    var AdminCtrl = function ($scope, $window, $routeParams, $location, $http) {
         
         $http.get('admin/users')
             .then(function (res) {
             $scope.Userlist = res.data;
         });
-
+        
         $scope.GetData = function () {
-              $http.get("admin/users/" + $scope.selectedUSER[0]._id + "/score")
+            $http.get("admin/users/" + $scope.selectedUSER[0]._id + "/score")
                     .then(function (res) {
-                    $scope.scorelist = res.data;
-                });
-                $http.get("admin/users/" + $scope.selectedUSER[0]._id + "/survey")
+                $scope.scorelist = res.data;
+            });
+            $http.get("admin/users/" + $scope.selectedUSER[0]._id + "/survey")
                     .then(function (res) {
                 $scope.vragenlist = res.data;
-                });
-            }
-           
-        $scope.AlgemeneAccountinstellingen = function () {            
+            });
+        }
+        
+        $scope.AlgemeneAccountinstellingen = function () {
             $location.path("/AlgemeneAccountinstellingen");
         }
-                    
+        
+        $scope.logout = function(){
+            var url = "http://" + $window.location.host + "/logout";
+            $window.location.href = url;
+        }
     };
     App.controller("AdminCtrl", AdminCtrl);
     
