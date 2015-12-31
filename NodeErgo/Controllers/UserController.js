1490c6b8b46720f267b4db1aa897082fba12d10b
V4mirax <kerstensbart.bk@gmail.com>
2015-12-31 14:47:52 +0100

    update bijgewerkt

--- NodeErgo/Controllers/UserController.js
+++ NodeErgo/Controllers/UserController.js
@@ -5,22 +5,21 @@ var auth = require('../Passport/auth.js');
 
 
 
-exports.create = function (req, res, next)
-{
+exports.create = function (req, res, next) {
     var userdata = req.body;
     
     if (
         userdata.APass == "Ergotherapie") {
         userdata.Admin = true;
     }
-
+    
     userdata.salt = encrypt.createSalt();
     userdata.hashed_pwd = encrypt.hashPwd(userdata.salt, userdata.password);
     
     var email = userdata.email;
     var username = userdata.username;
     
-    Users.findOne({'email': email},function myFunction(err,user) {
+    Users.findOne({ 'email': email }, function myFunction(err, user) {
         if (err) {
             console.log('Error in signup: ' + err);
             return res.send({ reason: err.toString() });
@@ -31,20 +30,20 @@ exports.create = function (req, res, next)
         }
         else {
             Users.findOne({ 'username': username }, function (err, user) {
-            if (err) {
+                if (err) {
                     console.log('Error in signup: ' + err);
                     return res.send({ reason: err.toString() });
-            }
-            if (user) {
+                }
+                if (user) {
                     console.log('user allready in use');
                     return res.redirect('/');//hier moet de view gewoon terug terecht komen van register
-            }
-              else {
+                }
+                else {
                     Users.create(userdata, function myFunction(err, user) {
                         
-                        req.logIn(user, function(err) {
+                        req.logIn(user, function (err) {
                             if (err) {
-                                console.log('something went wrong in the login'); 
+                                console.log('something went wrong in the login');
                             }
                             else {
                                 res.redirect('/Home');
@@ -56,14 +55,14 @@ exports.create = function (req, res, next)
                     
         
                     });
-              }
+                }
                
             
             });
         }
         
         
-    })   
+    })
 
    
    
@@ -73,12 +72,12 @@ exports.create = function (req, res, next)
 
 exports.getUserList = function (req, res) {
     Users.find({}, function (err, users) {
-             
+        
         if (err) {
             throw err;
         }
         else {
-           
+            
             console.log(users);
            
         }
@@ -88,28 +87,55 @@ exports.getUserList = function (req, res) {
 
 };
 
-exports.updateUser = function(req,res,next) {
+exports.updateUser = function (req, res, next) {
     var userUpdates = req.body;
-
+    var userdata = {};
+    
+    
+    console.log(userUpdates);
     if (req.user._id != userUpdates._id && !req.user.Admin) {
         res.send(401);
         return res.end();
     }
     
-    req.user.name = userUpdates.name;
+    //kijkt of naam is ingevuld en de lengte groter is dan 0
+    if (userUpdates.name && userUpdates.name.length > 0) {
+        req.user.name = userUpdates.name;
+        userdata.name = req.user.name;
+    }
+    //kijkt of passwoord is ingevuld en de lengte groter is dan 0
     if (userUpdates.password && userUpdates.password.length > 0) {
         req.user.salt = encrypt.createSalt();
         req.user.hashed_pwd = encrypt.hashPwd(req.user.salt, userUpdates.password);
     }
-
+    //kijkt of telefoon is ingevuld en groter is dan 0
+    if (userUpdates.telefoon && userUpdates.telefoon.length > 0) {
+        req.user.telefoon = userUpdates.telefoon;
+        userdata.telefoon = userUpdates.telefoon;
+    }
+    if (userUpdates.age && userUpdates.age.length > 0) {
+        req.user.age = userUpdates.age;
+        userdata.age = userUpdates.age;
+    }
+    //email aanpassen
+    
+    if (userUpdates.email && userUpdates.email.length > 0) {         
+            userdata.email = userUpdates.email;      
+        req.user.email = userUpdates.email;
+    }
     req.user.save(function (err) {
         if (err) {
             res.status(400);
             return res.send({ reason: err.toString() });
         }
-        
     });
+    Users.update({ 'user': req.user._id }, { $set: userdata }, { upsert: false }, function (err, saved) {
+        if (err) return console.log(err);
+        res.sendStatus(201);
+    })
+   
+   
     
-    //res.sendStatus(200);
+   
 
 };
