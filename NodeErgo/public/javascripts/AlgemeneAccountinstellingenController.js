2a27b4514cdf85619ff0555df19604599c7986ab
Stijn Schrauwen <stijnschrauwen1995@hotmail.com>
2016-01-01 19:13:30 +0100

    checken of een email adres al in gebruik is

--- NodeErgo/public/javascripts/AlgemeneAccountinstellingenController.js
+++ NodeErgo/public/javascripts/AlgemeneAccountinstellingenController.js
@@ -2,10 +2,28 @@
     var App = angular.module("App");
     var AACtrl = function ($scope, $routeParams,$http) {
         
+        $scope.EmailInvallid = false;
+
         $http.get('user/userinfo')
             .then(function (res) {
             $scope.userinfo = res.data;
         });
+
+        $http.get('admin/users')
+            .then(function (res) {
+            $scope.Userlist = res.data;
+        });
+        
+        $scope.Emailcheck = function (test) {
+            $scope.EmailInvallid = false;
+          for (count = 0; count < $scope.Userlist.length; count++) {
+               if ($scope.Userlist[count].email == test) {
+                  $scope.EmailInvallid = true;
+                }
+                }
+        }
+
+
     };
     App.controller("AACtrl", AACtrl);
     
