a3275728f8e650b57498f0660ab6dc3a1f8ae3bb
Stijn Schrauwen <stijnschrauwen1995@hotmail.com>
2016-01-04 20:07:35 +0100

    nieuwe pagina + controller

---
+++ NodeErgo/public/javascripts/AdminStartscreenController.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    var App = angular.module("App");
+    var AdminStartscreenCtrl = function ($scope, $window, $routeParams, $location) {
+                
+        $scope.Uitloggenclick = function () {
+            var url = "http://" + $window.location.host + "/logout";
+            $window.location.href = url;
+        }
+
+        $scope.Resultsclick = function () {
+             $location.path("/AllUsers");
+        }
+        $scope.Startclick = function () {
+            $location.path("/OneUser");
+        }
+        
+        $scope.Configclick = function () {
+            $location.path("/AlgemeneAccountinstellingen");
+        }
+
+        };
+    App.controller("AdminStartscreenCtrl", AdminStartscreenCtrl);
+
+    })();
--- NodeErgo/public/javascripts/AdminViewController.js
+++ NodeErgo/public/javascripts/AdminViewController.js
@@ -5,12 +5,19 @@
     App.config(function ($routeProvider){
         $routeProvider
         .when("/", {
-            templateUrl: "views/Admin.html",
-            controller: "AdminCtrl"
+            templateUrl: "views/AdminStartpage.html",
+            controller: "AdminStartscreenCtrl"
         })
         .when("/AlgemeneAccountinstellingen", {
             templateUrl: "views/AlgemeneAccountinstellingen.html",
             controller: "AACtrl"
+        })
+        .when("/OneUser", {
+            templateUrl: "views/Admin.html",
+            controller: "AdminCtrl"
+        })
+        .when("/AllUsers", {
+            //
         })
          .otherwise({ redirectTo: "/" });//indien pagina niet gevonden wordt
     });
