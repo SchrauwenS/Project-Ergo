407dcc54b4a269bd1592d1a77bf5b14fe141ae88
Stijn Schrauwen <stijnschrauwen1995@hotmail.com>
2016-01-04 19:38:56 +0100

    moet serverside gecheckt worden om user geen toegang te verschaffen tot restricted areas

--- NodeErgo/public/javascripts/AlgemeneAccountinstellingenController.js
+++ NodeErgo/public/javascripts/AlgemeneAccountinstellingenController.js
@@ -9,21 +9,6 @@
             $scope.userinfo = res.data;
         });
 
-        $http.get('admin/users')
-            .then(function (res) {
-            $scope.Userlist = res.data;
-        });
-        
-        $scope.Emailcheck = function (test) {
-            $scope.EmailInvallid = false;
-          for (count = 0; count < $scope.Userlist.length; count++) {
-               if ($scope.Userlist[count].email == test) {
-                  $scope.EmailInvallid = true;
-                }
-                }
-        }
-
-
     };
     App.controller("AACtrl", AACtrl);
     
