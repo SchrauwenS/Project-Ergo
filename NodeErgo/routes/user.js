6f53134762586481aeb44df3142c8db302673ddc
V4mirax <kerstensbart.bk@gmail.com>
2016-01-05 11:21:30 +0100

    added mail controll

--- NodeErgo/routes/user.js
+++ NodeErgo/routes/user.js
@@ -9,18 +9,48 @@ var user = schema.Users;
 var isLoggedIn = auth.requiresApiLogin;
 
 
+
+router.post('/Update', isLoggedIn, function (req, res, next) {
+    
+    var email = req.body.email;
+    console.log('huidige body:' + req.body.email);
+    
+    console.log('huidige aangemelde persoon: ' + req.user.email);
+    
+    user.findOne({ 'email': email }, function myFunction(err, user) {
+        if (err) {
+            console.log('Error in signup: ' + err);
+            return res.send({ reason: err.toString() });
+        }
+        if (user) {
+            if (req.user.email != email) {
+                res.status(401);
+                res.end();
+                return res.redirect('/');
+            }
+            else {
+                console.log('mail not changed');
+            }
+            
+        }
+        return UserC.updateUser(req, res, next);
+       
+    }
+    )
+});
+
 router.get('/userinfo', isLoggedIn, function (req, res) {
     
     var userID = req.user.id;
     console.log(userID);
-
+    
     user.findById(userID).lean().exec(function (err, result) {
         if (err) return console.error(err);
         
         var userdata = {
             userid: userID,
             username: result.username,
-            name:result.name,
+            name: result.name,
             email: result.email,
             telephone: result.telefoon,
             age: result.age,
@@ -34,7 +64,7 @@ router.get('/userinfo', isLoggedIn, function (req, res) {
 
         }
         
-         res.status(200).json(userdata);
+        res.status(200).json(userdata);
     })
 })
 
