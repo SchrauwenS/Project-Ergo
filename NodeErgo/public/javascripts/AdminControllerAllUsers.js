d21eaf7083a9bcd20c3fbfe7ad8d58efb1b1056a
Stijn Schrauwen <stijnschrauwen1995@hotmail.com>
2016-01-09 01:47:24 +0100

    multiparameter filter parameter kindere nog niet op punt

--- NodeErgo/public/javascripts/AdminControllerAllUsers.js
+++ NodeErgo/public/javascripts/AdminControllerAllUsers.js
@@ -2,11 +2,32 @@
     var App = angular.module("App");
     var AdminAllusersCtrl = function ($scope, $location, $http) {
         
-        var r = $.Deferred();
-        r.done(Filter);
-        var s = $.Deferred();
-        s.done(ToScreen);     
+        $scope.gender = "NoFilter";
+        $scope.diploma = "NoFilter";
+        $scope.BurgS = "NoFilter";
+        $scope.Origine = "NoFilter";
+        $scope.werkstatus = "NoFilter";
+        $scope.Kinderen = false;
+        $scope.KinderenMax = 10;
+        $scope.KinderenMin = 0;
+
+        var Signal = signals.Signal;
+        var myObject2 = {
+            started: new Signal(),
+            stopped: new Signal()
+        };
+        myObject2.started.add(function () {
+            $scope.Filter();
+        });
+        var myObject = {
+            started: new Signal(),
+            stopped: new Signal()
+        };
+        myObject.started.add(function () {
+            ToScreen(scorelistCalc, scorelistCalcNietsingevuld);
+        });
         
+                
         var Userlist;
         var scorelist;
         var filterlist;
@@ -19,13 +40,30 @@
         $http.get('admin/users')
             .then(function (res) {
             Userlist = res.data;
-            r.resolve();
+            myObject2.started.dispatch();
             });  
         
-        function Filter()
+         $scope.Filter = function()
         {
-        filterlist = $(Userlist).filter(function (i, n) { return n.geslacht === 'Male' && n.Admin === false });
             var count = 0;
+            scorelistCalcNietsingevuld = { subGezondheid: 0, subIdentiteit: 0, subRelaties: 0, subUitdaging: 0, TotaalScore: 0 };
+            scorelistCalc = { subGezondheid: 0, subIdentiteit: 0, subRelaties: 0, subUitdaging: 0, TotaalScore: 0 };
+            
+
+            filterlist = $(Userlist).filter(function (i, n) { return n.Admin === false });
+            if($scope.gender != "NoFilter")
+                filterlist = $(filterlist).filter(function (i, n) { return n.geslacht === $scope.gender });
+            if ($scope.diploma != "NoFilter")
+                filterlist = $(filterlist).filter(function (i, n) { return n.diploma === $scope.diploma });
+            if ($scope.BurgS != "NoFilter")
+                filterlist = $(filterlist).filter(function (i, n) { return n.burg_statuut  === $scope.BurgS  });
+            if ($scope.Origine != "NoFilter")
+                filterlist = $(filterlist).filter(function (i, n) { return n.huidskleur === $scope.Origine });
+            if ($scope.werkstatus != "NoFilter")
+                filterlist = $(filterlist).filter(function (i, n) { return n.werkstatus === $scope.werkstatus });
+            if ($scope.Kinderen != false)
+                filterlist = $(filterlist).filter(function (i, n) { return $scope.KinderenMin < n.kinderen < $scope.KinderenMax });
+
 
         for (var i = 0; i < filterlist.length; i++) {
                 $http.get("/admin/users/" + filterlist[i]._id + "/score")
@@ -34,8 +72,15 @@
                     calculateFilterScore();
                     count++
                     if (count == filterlist.length)
-                        s.resolve(scorelistCalc, scorelistCalcNietsingevuld);           
+                            myObject.started.dispatch();
                 });
+            }
+            if (filterlist.length == 0) { 
+                $scope.scorelistAll.subGezondheid = "n.v.t.";
+                $scope.scorelistAll.subIdentiteit = "n.v.t.";
+                $scope.scorelistAll.subRelaties = "n.v.t.";
+                $scope.scorelistAll.subUitdaging = "n.v.t.";
+                $scope.scorelistAll.TotaalScore = "n.v.t.";
             }
         }
         
@@ -74,8 +119,7 @@
             }      
         }
         
-        function ToScreen(scorelistCalc, scorelistCalcNietsingevuld) {
-                                                            
+        function ToScreen(scorelistCalc, scorelistCalcNietsingevuld) {                              
             $scope.scorelistAll.subGezondheid = Math.round(scorelistCalc.subGezondheid / (filterlist.length - scorelistCalcNietsingevuld.subGezondheid)* 100)/ 100;
             $scope.scorelistAll.subIdentiteit = Math.round(scorelistCalc.subIdentiteit / (filterlist.length - scorelistCalcNietsingevuld.subIdentiteit) * 100) / 100;
             $scope.scorelistAll.subRelaties = Math.round(scorelistCalc.subRelaties / (filterlist.length - scorelistCalcNietsingevuld.subRelaties) * 100) / 100;
@@ -83,6 +127,9 @@
             $scope.scorelistAll.TotaalScore = Math.round(scorelistCalc.TotaalScore / (filterlist.length - scorelistCalcNietsingevuld.TotaalScore) * 100) / 100;
         }
         
+        $scope.back = function () {
+            
+        }
 
         $scope.back = function () {
             $location.path("/");
